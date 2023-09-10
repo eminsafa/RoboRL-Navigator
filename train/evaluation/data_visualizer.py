2bd5bc3f78b7a3fde1311932927c21efa969aac6
Safa Tok <safa@citymapper.com>
2023-09-10 14:32:23 +0100

    model comparison

--- train/evaluation/data_visualizer.py
+++ train/evaluation/data_visualizer.py
@@ -3,18 +3,28 @@ import matplotlib.pyplot as plt
 
 
 # Read the CSV file into a DataFrame
-data = pd.read_csv('/models/roborl-navigator/model_comparison/TD3_HER_50K/logs/progress.csv')
+models = {
+    "TD3": {'i': 1, "marker": "o", "color": "blue"},
+    "SAC": {"i": 3, "marker": "s", "color": "red"},
+    "DDPG": {"i": 2, "marker": "^", "color": "green"},
+}
+plt.figure(figsize=(14, 6))
 
-# Extract the 'rollout/success_rate' column
-success_rate = data['rollout/success_rate']
+for model, d in models.items():
+    i = d['i']
+    plt.subplot(1, 3, i)
+    data = pd.read_csv(f'/Users/safa/qa/RoboRL-Navigator/models/roborl-navigator/model_comparison/{model}_HER_50K/logs/progress.csv')
+    # Extract the 'rollout/success_rate' column
 
-plt.figure(figsize=(10, 6))
-plt.plot(success_rate, marker='o', linestyle='-')
-
-plt.xlabel('Time/Episodes')
-plt.ylabel('Rollout Success Rate')
-plt.title('Rollout Success Rate Over Time/Episodes')
+    success_rate = data['rollout/success_rate']
+    plt.plot(success_rate, linestyle='-', color=d['color'])
+    plt.xlabel('Steps (K)')
+    plt.ylabel('Success Rate (%)')
+    plt.title(f'{model} Success Rate Over Time')
+    plt.xlim(0, 120)  # Set the X-axis limits to be from 1 to 5
+    plt.ylim(0, 1.05)
+    plt.grid(True)
 
 # Show the plot
-plt.grid(True)
+
 plt.show()
